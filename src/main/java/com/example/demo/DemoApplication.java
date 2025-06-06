ead37e1228a3043d7b582fe3d3a802b2d3104d8d
Ayana Yokota <ihcomega56@github.com>
2025-06-06 10:24:51 +0900

    JavaDocを追加した (#9)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Demoアプリケーションのメインクラス。
+ * Spring Bootアプリケーションを起動するためのエントリーポイントです。
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * アプリケーションのメインメソッド。
+     * Spring Bootアプリケーションを起動します。
+     *
+     * @param args コマンドライン引数
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
